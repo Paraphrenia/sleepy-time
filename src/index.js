5153f713dd684784ce39a71e6acad10274978fc2
ernieora <paraphrenia@protonmail.com>
2022-06-18 07:15:58 -0700

    Added the sleeping interval math and date grabbing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-const d = new Date();
-document.getElementById("demo").innerHTML = d.getSeconds();
+function dateGrabber() {
 
-console.log(d)
+    // Grabs full time and date from users computer
+    let d = new Date()
+    console.log(d)
+
+    // Adds 14 minutes
+    d.setMinutes(d.getMinutes() + 14);
+    console.log(d)
+
+    // Adds 90 minutes, 6 times
+    for (let i = 0; i < 6; i++) {
+    d.setMinutes(d.getMinutes() + 90);
+    console.log(d)
+    }
+}
