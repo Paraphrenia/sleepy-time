56610196bb57f68062e74e9a98e9d9908dc44525
ernieora <paraphrenia@protonmail.com>
2022-06-18 22:17:19 -0700

    Added console log to provide am/pm format

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 function dateGrabber() {
-
     // Grabs full time and date from users computer
     let d = new Date()
     console.log(d)
+
+
 
     // Adds 14 minutes
     d.setMinutes(d.getMinutes() + 14);
     console.log(d)
 
+
     // Adds 90 minutes, 6 times
-    for (let i = 0; i < 6; i++) {
-    d.setMinutes(d.getMinutes() + 90);
-    console.log(d)
-    }
+    for (let i = 0; i < 12; i++) {
+         d.setMinutes(d.getMinutes() + 90);
+         console.log(
+            d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+          );    }
 }
