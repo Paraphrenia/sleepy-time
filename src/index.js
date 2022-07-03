79bc45008beb3c82a2fda2bd347c40492db8064a
ernieora <paraphrenia@protonmail.com>
2022-07-03 14:11:58 -0700

    Set each button to switch to the proper display block div after clicking calculate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+function dateSet() {
+
+    document.getElementById('display').style.display = 'none'
+    document.getElementById('display-block-set').style.display = 'block'
+
+    // Grabs full time and date from users computer
+    let d = new Date()
+    console.log(d)
+
+
+    // Adds 14 minutes
+    d.setMinutes(d.getMinutes() + 14);
+    console.log(d)
+
+}
+
 function dateNow() {
 
     document.getElementById('display').style.display = 'none'
-    document.getElementById('display-block-click').style.display = 'block'
+    document.getElementById('display-block-now').style.display = 'block'
 
     // Grabs full time and date from users computer
     let d = new Date()
@@ -13,21 +29,9 @@ function dateNow() {
 
     // Adds 90 minutes, 6 times
     for (let i = 0; i < 6; i++) {
-         d.setMinutes(d.getMinutes() + 90);
-         console.log(
+        d.setMinutes(d.getMinutes() + 90);
+        console.log(
             d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-        );    
+        );
     }
 }
-
-function dateSet() {
-    // Grabs full time and date from users computer
-    let d = new Date()
-    console.log(d)
-
-
-    // Adds 14 minutes
-    d.setMinutes(d.getMinutes() + 14);
-    console.log(d)
-
-    }
