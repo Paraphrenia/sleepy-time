9ffcc242e46b9b886cfb5815161791128ac87ade
ernieora <paraphrenia@protonmail.com>
2022-07-17 11:23:12 -0700

    Added hour and minute grabber, and did the math for the sleeping time results

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function dateSet() {
 
+    // Hides main display and replaces with new one
     document.getElementById('display').style.display = 'none'
     document.getElementById('display-block-set').style.display = 'block'
 
@@ -7,15 +8,72 @@ function dateSet() {
     let d = new Date()
     console.log(d)
 
+    // Grabs hours from html option
+    const hours = document.getElementById("hours").options[document.getElementById("hours").selectedIndex].text
+    d.setHours(hours)
 
-    // Adds 14 minutes
-    d.setMinutes(d.getMinutes() + 14);
+    // Grabs minutes from html option
+    const minutes = document.getElementById("minutes").options[document.getElementById("minutes").selectedIndex].text
+    d.setMinutes(minutes)
+
+    // Grabs meridiem from html option
+    const meridiem = document.getElementById("meridiem").options[document.getElementById("meridiem").selectedIndex].text
+
+    // Prints time
     console.log(d)
+    const body = document.getElementById("print-set-results")
+    const timeSetText = document.createElement("h1")
+    timeSetText.textContent = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    body.append(timeSetText)
+    timeSetText.classList.add("result-container")
 
+    // New div to seperate info
+    const div = document.createElement("div")
+    body.append(div)
+
+    // Adds text to let user know the times below are to sleep
+    const sleepText = document.createElement("h1")
+    sleepText.textContent = "You should sleep at these following times"
+    body.append(sleepText)
+
+    // Displays math results for when user should sleep
+    // Subtracts 90 minutes, 6 times
+    for (let i = 0; i < 6; i++) {
+        d.setMinutes(d.getMinutes() - 90);
+        console.log(
+            d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+        );
+        const resultTime = document.createElement("h1")
+        resultTime.textContent = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+        body.append(resultTime)
+        resultTime.classList.add("result-container")
+    }
+
+    // New div to seperate info
+    const div1 = document.createElement("div")
+    body.append(div1)
+
+    // Adds disclaimer text
+    const cycles = document.createElement("h1")
+    cycles.textContent = ("It's best to get 5-6 sleep cycles.")
+    body.append(cycles)
+    
+
+    // Creates Go Home button
+    const button = document.createElement("button")
+    button.textContent = "Go Home"
+    body.append(button)
+    button.classList.add("btn-1", "btn-text")
+    button.id = "go-home"
+
+    document.getElementById("go-home").onclick = function () {
+        window.location.reload()
+    }
 }
 
 function dateNow() {
 
+    // Hides main display and replaces with new one
     document.getElementById('display').style.display = 'none'
     document.getElementById('display-block-now').style.display = 'block'
 
@@ -23,8 +81,8 @@ function dateNow() {
     let d = new Date()
     console.log(d)
 
-    // Adds 14 minutes
-    d.setMinutes(d.getMinutes() + 14);
+    // Adds 15 minutes
+    d.setMinutes(d.getMinutes() + 15);
     console.log(d)
 
     // Adds 90 minutes, 6 times
@@ -40,18 +98,20 @@ function dateNow() {
         h1.classList.add("result-container")
     }
 
+    // Prints 15 minutes disclaimer
     const body = document.getElementById("print-now-results")
     const h1 = document.createElement("h1")
-    h1.textContent = "It's best to get 5-6 cycles of sleep"
+    h1.textContent = "(The average time to fall asleep is 15 minutes)"
     body.append(h1)
 
+    // Creates Go Home button
     const button = document.createElement("button")
     button.textContent = "Go Home"
     body.append(button)
     button.classList.add("btn-1", "btn-text")
-    button.id="go-home"
+    button.id = "go-home"
 
-    document.getElementById("go-home").onclick = function() {
+    document.getElementById("go-home").onclick = function () {
         window.location.reload()
     }
 
